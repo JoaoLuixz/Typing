db4e654ee45886abda60eb65f5ed86e20568ad43
João Luiz <joaoluizmartins6@gmail.com>
2024-03-22 15:02:16 -0300

    add timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TypingGame';
+  timerRunning:Boolean = false;
+  timer: number = 0;
+  phrases: Array<Object> = [];
+  phrase!: string;
+  inputPhrase!: string;
+  apiResponse: any;
+  cronometer: any;
+  startedTyping = false;
+  constructor(private http: HttpClient){
+  http.get('https://api.quotable.io/random').subscribe(answer => {
+    console.log(answer);
+    this.apiResponse = answer;
+    this.phrase = this.apiResponse.content;
+    this.phrases.push(this.phrase);
+    this.startTyping();
+  })
 }
+ 
+ 
+  // Começa a contagem do cronômetro
+  startTyping(){
+    this.cronometer = setInterval(() => {
+      this.timer++
+    }, 1000)
+  }
+  
+  // Para o cronômetro
+  stopTyping(){
+    clearInterval(this.cronometer);
+  }
+  // Incío de ciclo da aplicação onde verifica se o usuário digitiu algo
+  ngOnInit(): void {
+    
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
