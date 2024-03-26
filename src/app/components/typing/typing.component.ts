38bdd25926ad5ce3292507f5196e643d18584a2a
João Luiz <joaoluizmartins6@gmail.com>
2024-03-26 12:00:22 -0300

    fixed timer and add comments

--- src/app/components/typing/typing.component.ts
+++ src/app/components/typing/typing.component.ts
@@ -9,40 +9,46 @@ import { EventManager } from '@angular/platform-browser';
   styleUrl: './typing.component.scss'
 })
 export class TypingComponent {
+  // Título da aplicação/componente
   title = 'TypingGame';
-  timerCount: number = 0;
+  // variável responsável por armazenar os segundos
+  timerSeconds: number = 0;
+  // variável resonsável por armazenar os minutos
   phrases: Array<{
     content: string,
     author: string
   }> = [];
+  // Objeto respnsável por armazenas as frases requisitadas
   phrase = {
     content: '',
     author: ''
   };
+  // variável responsável por armazenas a frase digitada pelo usuário
   inputPhrase!: string;
+  // variável respnsável por armazernar as palavras digitadas pelo usuário
   inputWords!: Array<string>;
+  // variável responsável por armazenar o resultado do cálculo da quantidade de palavras por minuto.
   wpm!: number;
+  // variável responsável por armazenas as informações recebidas da API de frases.
   apiResponse: any;
+  // variável responsável por amazernar o intervalo de tempo do cronômetro
   timer: any;
+  // variável reponsável por armazenar a quantidade máxima de caracteres que o usuáro deseja
   maxCharacters: number;
+  // variável responsável por armazenar o nome do usuário
   playerName: string;
+  // contrutor da classe TypingComponent
   constructor(private http: HttpClient, private sharedData: SharedDataService){
+    // instrução que define a quantidade máxima de caracteres baseado na esclha do usuário
     this.maxCharacters = this.sharedData.getMaxCharacters();
+    // intrução que define o nome digitado pelo usuário
     this.playerName = this.sharedData.getPlayerName();
-    this.http.get(`https://api.quotable.io/random?maxLength=${this.maxCharacters}`).subscribe(answer => {
-    console.log(answer);
-    this.apiResponse = answer;
-    this.phrase.content = this.apiResponse.content;
-    this.phrase.author = this.apiResponse.author;
-    this.phrases.push({
-      content: this.phrase.content,
-      author: this.phrase.author,
-    });
-  })
-  
+    // chamada do método que faz a requisição de uma nova frase
+    this.newPhrase();
+    // chamada do método que inicia o cronômetro
     this.startTyping();
 }
-
+// Meteodo que faz a requisição de uma nova frase.
  newPhrase(){
   this.http.get(`https://api.quotable.io/random?maxLength=${this.maxCharacters}`).subscribe(answer => {
     console.log(answer);
@@ -54,28 +60,30 @@ export class TypingComponent {
       author: this.phrase.author,
     });
     this.inputPhrase = "";
+    this.stopTyping();
     this.startTyping();
   })
   
-  this.timerCount = 0;
+  this.timerSeconds = 0;
  }
+ // Metedodo que executa o calculo de pralavras por minuto
  calculateWPM():number{
   this.inputWords = this.inputPhrase.split(" ");
-  return ((this.inputWords.length) / (this.timerCount / 60));
+  return ((this.inputWords.length) / (this.timerSeconds / 60));
 }
-  // Começa a contagem do cronômetro
+  // Metodo que inicia a contagem do cronômetro
   startTyping(){
     this.timer = setInterval(() => {
-      if(this.timerCount == 60){
-        this.timerCount = 1;
+      if(this.timerSeconds == 60){
+        this.timerSeconds = 1;
       }
       else{
-        this.timerCount++
+        this.timerSeconds++
       }
     }, 1000)
   }
   
-  // Para o cronômetro
+  // Metdodo que termina a contagem do cronômetro
   stopTyping(){
     clearInterval(this.timer);
     this.wpm = this.calculateWPM();
--- src/app/pipes/timer/timer.pipe.ts
+++ src/app/pipes/timer/timer.pipe.ts
@@ -5,19 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimerPipe implements PipeTransform {
   min: number = 0;
-  time!: number;
   transform(value: number, ...args: unknown[]): unknown {
     if(value == 60) {
       this.min++;
-      value = 1;
+      value = 1
     }
     if(value == 0){
       this.min = 0;
     }
-
-    return `${this.min}"${value}'`;
-
-    //return value > 60 ? `${value / 60}s` : `${value}s`;
+    return `${this.min}:${value}`;
   }
 
 }
